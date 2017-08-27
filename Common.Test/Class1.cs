4cd960cd05250e11f52165ade3ef4c5f47e9d0ba
MINGYI WANG <mingyi.frank@outlook.com>
2017-08-27 19:38:30 +0800

    add Test Project

---
+++ Common.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Common.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ EL.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EL.Test
+{
+    public class Class1
+    {
+    }
+}
