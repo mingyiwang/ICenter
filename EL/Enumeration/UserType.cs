b7aca39a918fb8274e94ea11f72e56ec4154c513
MINGYI WANG <mingyi.frank@outlook.com>
2017-09-04 19:10:49 +0800

    Updates

--- EL/Enumeration/UserType.cs
+++ EL/Enumeration/UserType.cs
@@ -2,6 +2,12 @@
 {
     public class UserType
     {
+
+        public static UserType Company = new UserType();
+        public static UserType Person  = new UserType();
+
+        private int _type;
+        public int Type => _type;
         
     }
 }
