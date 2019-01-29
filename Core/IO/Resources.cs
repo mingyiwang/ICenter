cde4a245ad6e0331468122e733001635c43efc1f
Frank <mingyi.frank@gmail.com>
2019-01-29 17:48:05 +0800

    updates

--- Core/IO/Resources.cs
+++ Core/IO/Resources.cs
@@ -9,18 +9,24 @@ namespace Core.IO
          
         public static byte[] GetBytes(Type type, string resourceName)
         {
-            return Streams.GetBytes(GetStream(type, resourceName));
+            using (var s = GetStream(type, resourceName))
+            {
+                return Streams.GetBytes(s);
+            }
         }
 
         public static string GetString(Type type, string resourceName)
         {
-            return Streams.GetString(GetStream(type, resourceName));
+            using (var s = GetStream(type, resourceName))
+            {
+                return Streams.GetString(s);
+            }
         }
 
         private static Stream GetStream(Type type, string resourceName)
         {
+            Checks.NotNullOrEmpty(resourceName);
             Checks.NotNull(type, "Type can not be null.");
-            Checks.NotNullOrEmpty("Resource name can not be null or empty.");
             return type.Assembly.GetManifestResourceStream(type, resourceName);
         }
 
